a97266371a20c9ce292fafe29681161248b8954b
Gurpreet Singh <singhg469@gmail.com>
2020-07-06 08:31:49 +0530

    Updates

---
+++ precache-manifest.c356bb860c5dc084086fcb2d4e27c97a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d523396faf2dde57743359e49d61b0be",
+    "url": "/my/index.html"
+  },
+  {
+    "revision": "39ce21328e8cf6710962",
+    "url": "/my/static/css/main.ec38d4ec.chunk.css"
+  },
+  {
+    "revision": "ec35f35c47cbe48a15ec",
+    "url": "/my/static/js/2.9acb5428.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/my/static/js/2.9acb5428.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39ce21328e8cf6710962",
+    "url": "/my/static/js/main.63650436.chunk.js"
+  },
+  {
+    "revision": "c2fa64e4f0a44d0fa1e4",
+    "url": "/my/static/js/runtime-main.ceb7704b.js"
+  }
+]);
